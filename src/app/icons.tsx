da052b73f7918e25d1383b5a30a5ccfcaf6d4a52
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-08 23:45:19 +0000

    Create icons.tsx

---
+++ src/app/icons.tsx
@@ -0,0 +1,113 @@
+
+import React from 'react';
+
+export default function Component() {
+  return (
+    <div className="h-screen w-16 flex flex-col items-stretch border-r px-2 overflow-hidden">
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          alt="Avatar"
+          className="rounded-full object-cover border-2 border-gray-100 w-10 h-10 dark:border-gray-850"
+          height={40}
+          src="/placeholder.svg"
+          style={{
+            aspectRatio: "40/40",
+            objectFit: "cover",
+          }}
+          width={40}
+        />
+      </div>
+    </div>
+  )
+}
