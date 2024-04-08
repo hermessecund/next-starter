601240180a1f838a94d47300735e2481d27c41cc
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-08 02:38:56 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import { client } from "./client";
+import { client, useSidebarState } from "./client";
 import Logo from "@public/logo.png";
 import { ConnectButton } from "@/app/thirdweb";
 import NavLinks from "./nav";
