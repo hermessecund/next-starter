4c797dfde8f1fccdde26b3f058856b9a8f06b7ab
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-08 02:37:14 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,43 @@ import Image from "next/image";
 import { client } from "./client";
 import Logo from "@public/logo.png";
 import { ConnectButton } from "@/app/thirdweb";
+import NavLinks from "./nav";
 
+export default function Header() {
+    const [showNavLinks, setShowNavLinks] = useState(false);
+
+    const toggleNavLinks = () => {
+        setShowNavLinks(!showNavLinks);
+    };
+    
 export default function Home() {
     return (
         <div className="relative min-h-screen">
             {/* Header */}
-            <header className="flex justify-between items-center p-4 bg-gray-900 text-white">
-                <div>
-                    <Image src={Logo} alt="Logo" />
-                </div>
-                    {/* NFT - Token Button */}
-                    <button className="text-white bg-gray-200 px-4 py-2 rounded-md">
-                        NFT - Token
-                    </button>
-                   
-                <div className="flex space-x-4">
-                    {/* Connect Button */}
-                    <ConnectButton
-                        client={client}
-                        appMetadata={{
-                            name: "Tar App",
-                            url: "https://tarwar.com",
-                        }}
-                    />
-                </div>
-            </header>
+              <header className="flex justify-between items-center p-4 bg-gray-900 text-white">
+            <div className="flex items-center">
+                <button onClick={toggleNavLinks} className="mr-4 focus:outline-none">
+                    {/* Small logo */}
+                    <Image src={Logo} alt="Logo" width={32} height={32} />
+                </button>
+                {/* NFT - Token Button */}
+                <button className="text-white bg-gray-200 px-4 py-2 rounded-md mr-4">
+                    NFT - Token
+                </button>
+            </div>
+            <div className="flex space-x-4">
+                {/* Connect Button */}
+                <ConnectButton
+                    client={client}
+                    appMetadata={{
+                        name: "Tar App",
+                        url: "https://tarwar.com",
+                    }}
+                />
+            </div>
+            {/* Render NavLinks component only when showNavLinks is true */}
+            {showNavLinks && <NavLinks />}
+        </header>
 
             {/* Main Content */}
             <main className="p-4 overflow-y-auto">
