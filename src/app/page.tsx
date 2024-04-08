a366dc268e6c3679ae8255916f88d9a507e1cce5
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-08 01:23:27 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import Image from "next/image";
 import { client, useSidebarState } from "./client";
 import thirdwebIcon from "@public/thirdweb.svg";
@@ -10,7 +9,7 @@ export default function Home() {
     const { sidebarVisible, toggleSidebar } = useSidebarState();
 
     return (
-        <div className="flex flex-col h-screen">
+        <div className="relative min-h-screen">
             {/* Header */}
             <header className="flex justify-between items-center p-4 bg-gray-900 text-white">
                 <div className="cursor-pointer" onClick={toggleSidebar}>
@@ -28,25 +27,22 @@ export default function Home() {
                 </div>
             </header>
 
-            {/* Main Content */}
-            <main className="flex flex-1 overflow-hidden">
-                {/* Sidebar */}
-                <aside className={`bg-gray-800 text-white w-64 flex-shrink-0 overflow-y-auto transition-transform duration-300 transform ${sidebarVisible ? 'translate-x-0' : '-translate-x-full'}`}>
-                    <NavLinks />
-                </aside>
+            {/* Sidebar */}
+            <aside className={`bg-transparent text-white w-64 absolute top-0 left-0 h-full overflow-y-auto transition-transform duration-300 transform ${sidebarVisible ? 'translate-x-0' : '-translate-x-full'}`}>
+                <NavLinks />
+            </aside>
 
-                {/* Main Content */}
-                <section className="flex-1 p-4 overflow-y-auto">
-                    {/* Responsive iframe */}
-                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%' }}>
-                        <iframe
-                            src="https://harmonysegment.com/"
-                            frameBorder="0"
-                            allowFullScreen
-                            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                        ></iframe>
-                    </div>
-                </section>
+            {/* Main Content */}
+            <main className="p-4 overflow-y-auto">
+                {/* Responsive iframe */}
+                <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%' }}>
+                    <iframe
+                        src="https://harmonysegment.com/"
+                        frameBorder="0"
+                        allowFullScreen
+                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                    ></iframe>
+                </div>
             </main>
 
             {/* Footer */}
@@ -56,3 +52,4 @@ export default function Home() {
         </div>
     );
 }
+
