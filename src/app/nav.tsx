c894d68682504a0b53cdfeeaef2889dccbe38c48
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-08 01:01:39 +0000

    Update nav.tsx

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function NavLinks() {
   return (
-<nav class="flex flex-col h-[calc(100vh-4rem)] w-16 min-h-[400px] border-r border-gray-200 px-2 py-4 shrink-0 gap-4 md:flex-row md:w-64 xl:w-80/60 2xl:w-96/60 border-gray-200 dark:border-gray-800">
-  <div class="flex items-center space-y-0.5">
-    <a class="flex w-full items-center" href="#">
+<nav className="flex flex-col h-[calc(100vh-4rem)] w-16 min-h-[400px] border-r border-gray-200 px-2 py-4 shrink-0 gap-4 md:flex-row md:w-64 xl:w-80/60 2xl:w-96/60 border-gray-200 dark:border-gray-800">
+  <div className="flex items-center space-y-0.5">
+    <a className="flex w-full items-center" href="#">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -15,20 +15,20 @@ export default function NavLinks() {
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
-        class="w-6 h-6"
+        className="w-6 h-6"
       >
         <path d="m7.5 4.27 9 5.15"></path>
         <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"></path>
         <path d="m3.3 7 8.7 5 8.7-5"></path>
         <path d="M12 22V12"></path>
       </svg>
-      <span class="sr-only">Logo</span>
+      <span className="sr-only">Logo</span>
     </a>
   </div>
-  <div class="flex-1 flex flex-col justify-between min-h-0">
-    <div class="flex-1 grid flex-auto min-h-0 shrink-0 items-start gap-4">
+  <div className="flex-1 flex flex-col justify-between min-h-0">
+    <div className="flex-1 grid flex-auto min-h-0 shrink-0 items-start gap-4">
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-gray-100 px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100/50 focus:bg-gray-100/50 focus:outline-none"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-gray-100 px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100/50 focus:bg-gray-100/50 focus:outline-none"
         href="#"
       >
         <svg
@@ -41,7 +41,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
           <polyline points="9 22 9 12 15 12 15 22"></polyline>
@@ -49,7 +49,7 @@ export default function NavLinks() {
         Home
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -62,7 +62,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <line x1="4" x2="4" y1="21" y2="14"></line>
           <line x1="4" x2="4" y1="10" y2="3"></line>
@@ -77,7 +77,7 @@ export default function NavLinks() {
         Dashboard
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -90,7 +90,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
           <circle cx="9" cy="7" r="4"></circle>
@@ -100,7 +100,7 @@ export default function NavLinks() {
         Team
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -113,7 +113,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="m7.5 4.27 9 5.15"></path>
           <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"></path>
@@ -123,7 +123,7 @@ export default function NavLinks() {
         Projects
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -136,7 +136,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
           <line x1="16" x2="16" y1="2" y2="6"></line>
@@ -146,7 +146,7 @@ export default function NavLinks() {
         Calendar
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -159,14 +159,14 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
         </svg>
         Messages
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -179,7 +179,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
           <circle cx="12" cy="12" r="3"></circle>
@@ -187,7 +187,7 @@ export default function NavLinks() {
         Settings
       </a>
       <a
-        class="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
+        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none data-[active]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50"
         href="#"
       >
         <svg
@@ -200,7 +200,7 @@ export default function NavLinks() {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="mr-2 h-4 w-4"
+          className="mr-2 h-4 w-4"
         >
           <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
           <circle cx="12" cy="7" r="4"></circle>
@@ -216,7 +216,7 @@ export default function NavLinks() {
 function NavLink({ href, children }) {
   return (
     <Link href={href}>
-      <a className="inline-flex h-9 w-max items-center justify-center rounded-md bg-gray-100 px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100/50 focus:bg-gray-100/50 focus:outline-none">
+      <a classNameName="inline-flex h-9 w-max items-center justify-center rounded-md bg-gray-100 px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100/50 focus:bg-gray-100/50 focus:outline-none">
         {children}
       </a>
     </Link>
