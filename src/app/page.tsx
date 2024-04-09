2f20c7a4f4b7b63cf3af7d8a47ed6427fc7a58d9
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-09 03:26:38 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,13 @@ export default function Header() {
     };
     return (
        <div className="relative min-h-screen">
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-    <div style={{ width: '200px', height: '200px', backgroundColor: 'lightgray' }}>
-        {/* Your content here */}
+
+      
  
-            <iframe src="https://next-starter-rouge-five.vercel.app/days.html" title="Days Iframe" height="70" frameBorder="0" style={{ minWidth: "150px" }}></iframe>
-        </div>
-        </div>
+      
+
     <header className="flex flex-col md:flex-row justify-center items-center p-4 bg-black text-white">
+              <iframe src="https://next-starter-rouge-five.vercel.app/days.html" title="Days Iframe" height="70" frameBorder="0" style={{ minWidth: "150px" }}></iframe>
         <div className="flex items-center md:order-1">
             <button className="mr-4 focus:outline-none">
                 {/* Small logo */}
