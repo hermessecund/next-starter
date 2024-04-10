aecf0e9953f8908cf8423d32ee066c31985c8b1c
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-10 01:22:48 +0000

    Create token.tsx

---
+++ src/app/token.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tokens: React.FC = () => {
+    return (
+        <div>
+            {/* Your Token component content */}
+            <h2> Tokens Component</h2>
+            <p>This is the content of the Token component.</p>
+        </div>
+    );
+};
+
+export default Tokens;
