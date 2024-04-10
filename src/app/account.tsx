98701e10f0bae919290d0f351766d06d40a91d00
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-10 01:23:46 +0000

    Create account.tsx

---
+++ src/app/account.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Accounts: React.FC = () => {
+    return (
+        <div>
+            {/* Your Token component content */}
+            <h2>Accounts Component</h2>
+            <p>This is the content of the Accounts component.</p>
+        </div>
+    );
+};
+
+export default Accounts;
