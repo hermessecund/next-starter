ed2869025c192b45eb94327663b0bc0c97315871
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-10 01:22:05 +0000

    Create nft.tsx

---
+++ src/app/nft.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nfts: React.FC = () => {
+    return (
+        <div>
+            {/* Your Token component content */}
+            <h2>Nft Component</h2>
+            <p>This is the content of the Token component.</p>
+        </div>
+    );
+};
+
+export default Nfts;
